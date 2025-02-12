beba58636405910e2ddb1370dea0f935e82c54b6
Gustavoa0388 <gustavoa18@gmail.com>
2025-02-12 20:21:16 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace DocumentosOrtobio
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxSearch1;
+        private System.Windows.Forms.TextBox textBoxSearch2;
+        private System.Windows.Forms.Button buttonSearch1;
+        private System.Windows.Forms.Button buttonSearch2;
+        private System.Windows.Forms.ListBox listBoxFiles1;
+        private System.Windows.Forms.ListBox listBoxFiles2;
+        private PdfiumViewer.PdfViewer pdfViewer1;
+        private PdfiumViewer.PdfViewer pdfViewer2;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.ComboBox comboBoxCategory1;
+        private System.Windows.Forms.ComboBox comboBoxSubCategory1;
+        private System.Windows.Forms.ComboBox comboBoxCategory2;
+        private System.Windows.Forms.ComboBox comboBoxSubCategory2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxSearch1 = new System.Windows.Forms.TextBox();
+            this.textBoxSearch2 = new System.Windows.Forms.TextBox();
+            this.buttonSearch1 = new System.Windows.Forms.Button();
+            this.buttonSearch2 = new System.Windows.Forms.Button();
+            this.listBoxFiles1 = new System.Windows.Forms.ListBox();
+            this.listBoxFiles2 = new System.Windows.Forms.ListBox();
+            this.pdfViewer1 = new PdfiumViewer.PdfViewer();
+            this.pdfViewer2 = new PdfiumViewer.PdfViewer();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.comboBoxCategory1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxSubCategory1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxCategory2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxSubCategory2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBoxSearch1
+            // 
+            this.textBoxSearch1.Location = new System.Drawing.Point(12, 43);
+            this.textBoxSearch1.Name = "textBoxSearch1";
+            this.textBoxSearch1.Size = new System.Drawing.Size(142, 20);
+            this.textBoxSearch1.TabIndex = 0;
+            // 
+            // textBoxSearch2
+            // 
+            this.textBoxSearch2.Location = new System.Drawing.Point(852, 39);
+            this.textBoxSearch2.Name = "textBoxSearch2";
+            this.textBoxSearch2.Size = new System.Drawing.Size(142, 20);
+            this.textBoxSearch2.TabIndex = 1;
+            this.textBoxSearch2.TextChanged += new System.EventHandler(this.TextBoxSearch2_TextChanged);
+            // 
+            // buttonSearch1
+            // 
+            this.buttonSearch1.Location = new System.Drawing.Point(160, 42);
+            this.buttonSearch1.Name = "buttonSearch1";
+            this.buttonSearch1.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch1.TabIndex = 2;
+            this.buttonSearch1.Text = "Buscar 1";
+            this.buttonSearch1.UseVisualStyleBackColor = true;
+            this.buttonSearch1.Click += new System.EventHandler(this.ButtonSearch1_Click);
+            // 
+            // buttonSearch2
+            // 
+            this.buttonSearch2.Location = new System.Drawing.Point(1000, 37);
+            this.buttonSearch2.Name = "buttonSearch2";
+            this.buttonSearch2.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch2.TabIndex = 3;
+            this.buttonSearch2.Text = "Buscar 2";
+            this.buttonSearch2.UseVisualStyleBackColor = true;
+            this.buttonSearch2.Click += new System.EventHandler(this.ButtonSearch2_Click);
+            // 
+            // listBoxFiles1
+            // 
+            this.listBoxFiles1.FormattingEnabled = true;
+            this.listBoxFiles1.Location = new System.Drawing.Point(12, 71);
+            this.listBoxFiles1.Name = "listBoxFiles1";
+            this.listBoxFiles1.Size = new System.Drawing.Size(142, 901);
+            this.listBoxFiles1.TabIndex = 4;
+            this.listBoxFiles1.SelectedIndexChanged += new System.EventHandler(this.ListBoxFiles1_SelectedIndexChanged);
+            // 
+            // listBoxFiles2
+            // 
+            this.listBoxFiles2.FormattingEnabled = true;
+            this.listBoxFiles2.Location = new System.Drawing.Point(852, 71);
+            this.listBoxFiles2.Name = "listBoxFiles2";
+            this.listBoxFiles2.Size = new System.Drawing.Size(142, 888);
+            this.listBoxFiles2.TabIndex = 5;
+            this.listBoxFiles2.SelectedIndexChanged += new System.EventHandler(this.ListBoxFiles2_SelectedIndexChanged);
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.AutoSize = true;
+            this.pdfViewer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pdfViewer1.Location = new System.Drawing.Point(160, 71);
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.Size = new System.Drawing.Size(675, 900);
+            this.pdfViewer1.TabIndex = 6;
+            // 
+            // pdfViewer2
+            // 
+            this.pdfViewer2.AutoSize = true;
+            this.pdfViewer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pdfViewer2.Location = new System.Drawing.Point(1000, 71);
+            this.pdfViewer2.Name = "pdfViewer2";
+            this.pdfViewer2.Size = new System.Drawing.Size(675, 900);
+            this.pdfViewer2.TabIndex = 7;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(1597, 10);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(75, 23);
+            this.btnLogout.TabIndex = 9;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.BtnLogout_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(1516, 10);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(75, 23);
+            this.btnSettings.TabIndex = 10;
+            this.btnSettings.Text = "Configurações";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.BtnSettings_Click);
+            // 
+            // comboBoxCategory1
+            // 
+            this.comboBoxCategory1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCategory1.FormattingEnabled = true;
+            this.comboBoxCategory1.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxCategory1.Name = "comboBoxCategory1";
+            this.comboBoxCategory1.Size = new System.Drawing.Size(142, 21);
+            this.comboBoxCategory1.TabIndex = 11;
+            this.comboBoxCategory1.SelectedIndexChanged += new System.EventHandler(this.ComboBoxCategory_SelectedIndexChanged);
+            // 
+            // comboBoxSubCategory1
+            // 
+            this.comboBoxSubCategory1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSubCategory1.FormattingEnabled = true;
+            this.comboBoxSubCategory1.Location = new System.Drawing.Point(160, 12);
+            this.comboBoxSubCategory1.Name = "comboBoxSubCategory1";
+            this.comboBoxSubCategory1.Size = new System.Drawing.Size(142, 21);
+            this.comboBoxSubCategory1.TabIndex = 12;
+            // 
+            // comboBoxCategory2
+            // 
+            this.comboBoxCategory2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCategory2.FormattingEnabled = true;
+            this.comboBoxCategory2.Location = new System.Drawing.Point(852, 12);
+            this.comboBoxCategory2.Name = "comboBoxCategory2";
+            this.comboBoxCategory2.Size = new System.Drawing.Size(142, 21);
+            this.comboBoxCategory2.TabIndex = 13;
+            this.comboBoxCategory2.SelectedIndexChanged += new System.EventHandler(this.ComboBoxCategory_SelectedIndexChanged);
+            // 
+            // comboBoxSubCategory2
+            // 
+            this.comboBoxSubCategory2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSubCategory2.FormattingEnabled = true;
+            this.comboBoxSubCategory2.Location = new System.Drawing.Point(1000, 12);
+            this.comboBoxSubCategory2.Name = "comboBoxSubCategory2";
+            this.comboBoxSubCategory2.Size = new System.Drawing.Size(139, 21);
+            this.comboBoxSubCategory2.TabIndex = 14;
+            this.comboBoxSubCategory2.SelectedIndexChanged += new System.EventHandler(this.ComboBoxSubCategory2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1684, 1031);
+            this.Controls.Add(this.comboBoxSubCategory2);
+            this.Controls.Add(this.comboBoxCategory2);
+            this.Controls.Add(this.comboBoxSubCategory1);
+            this.Controls.Add(this.comboBoxCategory1);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.pdfViewer2);
+            this.Controls.Add(this.pdfViewer1);
+            this.Controls.Add(this.listBoxFiles2);
+            this.Controls.Add(this.listBoxFiles1);
+            this.Controls.Add(this.buttonSearch2);
+            this.Controls.Add(this.buttonSearch1);
+            this.Controls.Add(this.textBoxSearch2);
+            this.Controls.Add(this.textBoxSearch1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Documentos Ortobio";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using PdfiumViewer;
+
+namespace DocumentosOrtobio
+{
+    public partial class Form1 : Form
+    {
+        private readonly User loggedUser;
+        private string currentFilePath1;
+        private string currentFilePath2;
+
+        private readonly Dictionary<string, List<string>> userPermissions;
+
+        private readonly Dictionary<string, List<string>> categoriesWithSubmenus = new Dictionary<string, List<string>>
+        {
+            { "Documentos Vigentes", new List<string> { "DT", "EC", "EMF", "GR", "NP", "RM", "RMP", "SF" } },
+            { "Documentos Obsoletos", new List<string> { "DT", "EC", "EMF", "GR", "NP", "RM", "RMP", "SF" } }
+        };
+
+        public Form1(User user)
+        {
+            InitializeComponent();
+            loggedUser = user;
+            userPermissions = LoadUserPermissions();
+
+            LogActivity("Login");
+
+            btnSettings.Visible = loggedUser.Role == "admin";
+            ConfigurePdfViewerPermissions();
+            PopulateCategoryComboBox(comboBoxCategory1);
+            PopulateCategoryComboBox(comboBoxCategory2);
+        }
+
+        private Dictionary<string, List<string>> LoadUserPermissions()
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(File.ReadAllText("userPermissions.json"));
+        }
+
+        private void ConfigurePdfViewerPermissions()
+        {
+            if (loggedUser.Role != "admin")
+            {
+                pdfViewer1.ShowToolbar = false;
+                pdfViewer2.ShowToolbar = false;
+            }
+        }
+
+        private void PopulateCategoryComboBox(ComboBox comboBox)
+        {
+            comboBox.Items.Clear();
+            comboBox.Items.Add("All Categories");
+
+            foreach (var category in categoriesWithSubmenus.Keys)
+            {
+                if (UserCanAccessCategory(category))
+                {
+                    comboBox.Items.Add(category);
+                }
+            }
+
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void ComboBoxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            if (comboBox != null)
+            {
+                var subCategoryComboBox = comboBox == comboBoxCategory1 ? comboBoxSubCategory1 : comboBoxSubCategory2;
+                PopulateSubCategoryComboBox(subCategoryComboBox, comboBox.SelectedItem.ToString());
+            }
+        }
+
+        private void PopulateSubCategoryComboBox(ComboBox comboBox, string category)
+        {
+            comboBox.Items.Clear();
+            comboBox.Items.Add("All Subcategories");
+
+            if (categoriesWithSubmenus.ContainsKey(category))
+            {
+                foreach (var subCategory in categoriesWithSubmenus[category])
+                {
+                    if (UserCanAccessSubCategory(category, subCategory))
+                    {
+                        comboBox.Items.Add(subCategory);
+                    }
+                }
+            }
+
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void ButtonSearch1_Click(object sender, EventArgs e)
+        {
+            LogActivity("Buscou na Categoria 1");
+            string searchPattern = textBoxSearch1.Text;
+            string selectedCategory = comboBoxCategory1.SelectedItem.ToString();
+            string selectedSubCategory = comboBoxSubCategory1.SelectedItem.ToString();
+            listBoxFiles1.Items.Clear();
+
+            if (selectedCategory == "All Categories")
+            {
+                foreach (var category in categoriesWithSubmenus.Keys)
+                {
+                    if (UserCanAccessCategory(category))
+                    {
+                        SearchFiles(category, selectedSubCategory, searchPattern, listBoxFiles1);
+                    }
+                }
+            }
+            else
+            {
+                SearchFiles(selectedCategory, selectedSubCategory, searchPattern, listBoxFiles1);
+            }
+        }
+
+        private void ButtonSearch2_Click(object sender, EventArgs e)
+        {
+            LogActivity("Buscou na Categoria 2");
+            string searchPattern = textBoxSearch2.Text;
+            string selectedCategory = comboBoxCategory2.SelectedItem.ToString();
+            string selectedSubCategory = comboBoxSubCategory2.SelectedItem.ToString();
+            listBoxFiles2.Items.Clear();
+
+            if (selectedCategory == "All Categories")
+            {
+                foreach (var category in categoriesWithSubmenus.Keys)
+                {
+                    if (UserCanAccessCategory(category))
+                    {
+                        SearchFiles(category, selectedSubCategory, searchPattern, listBoxFiles2);
+                    }
+                }
+            }
+            else
+            {
+                SearchFiles(selectedCategory, selectedSubCategory, searchPattern, listBoxFiles2);
+            }
+        }
+
+        private void SearchFiles(string category, string subCategory, string searchPattern, ListBox listBox)
+        {
+            if (subCategory == "All Subcategories")
+            {
+                foreach (var subCat in categoriesWithSubmenus[category])
+                {
+                    if (UserCanAccessSubCategory(category, subCat))
+                    {
+                        string subCategoryPath = Path.Combine(@"\\ntortobio\Central\Arquivos Diversos\Instalação\Documentos", category, subCat);
+                        string[] files = Directory.GetFiles(subCategoryPath, "*" + searchPattern + "*.pdf", SearchOption.AllDirectories);
+
+                        foreach (string file in files)
+                        {
+                            listBox.Items.Add(Path.GetFileName(file));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (UserCanAccessSubCategory(category, subCategory))
+                {
+                    string subCategoryPath = Path.Combine(@"\\ntortobio\Central\Arquivos Diversos\Instalação\Documentos", category, subCategory);
+                    string[] files = Directory.GetFiles(subCategoryPath, "*" + searchPattern + "*.pdf", SearchOption.AllDirectories);
+
+                    foreach (string file in files)
+                    {
+                        listBox.Items.Add(Path.GetFileName(file));
+                    }
+                }
+            }
+        }
+
+        private void ListBoxFiles1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxFiles1.SelectedItem != null)
+            {
+                string selectedFileName = listBoxFiles1.SelectedItem.ToString();
+                currentFilePath1 = GetFilePath(selectedFileName);
+                DisplayPdf1(currentFilePath1);
+                LogActivity($"Visualizou o arquivo {selectedFileName} na Categoria 1");
+            }
+        }
+
+        private void ListBoxFiles2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxFiles2.SelectedItem != null)
+            {
+                string selectedFileName = listBoxFiles2.SelectedItem.ToString();
+                currentFilePath2 = GetFilePath(selectedFileName);
+                DisplayPdf2(currentFilePath2);
+                LogActivity($"Visualizou o arquivo {selectedFileName} na Categoria 2");
+            }
+        }
+
+        private string GetFilePath(string fileName)
+        {
+            foreach (var category in categoriesWithSubmenus.Keys)
+            {
+                string categoryPath = Path.Combine(@"\\ntortobio\Central\Arquivos Diversos\Instalação\Documentos", category);
+                var files = Directory.GetFiles(categoryPath, fileName, SearchOption.AllDirectories);
+                if (files.Any())
+                {
+                    return files.First();
+                }
+                foreach (var subCategory in categoriesWithSubmenus[category])
+                {
+                    var subCategoryPath = Path.Combine(categoryPath, subCategory);
+                    files = Directory.GetFiles(subCategoryPath, fileName, SearchOption.AllDirectories);
+                    if (files.Any())
+                    {
+                        return files.First();
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void DisplayPdf1(string filePath)
+        {
+            if (filePath != null)
+            {
+                var pdfDocument = PdfDocument.Load(filePath);
+                pdfViewer1.Document = pdfDocument;
+            }
+        }
+
+        private void DisplayPdf2(string filePath)
+        {
+            if (filePath != null)
+            {
+                var pdfDocument = PdfDocument.Load(filePath);
+                pdfViewer2.Document = pdfDocument;
+            }
+        }
+
+        private bool UserCanAccessCategory(string category)
+        {
+            return userPermissions.ContainsKey(loggedUser.Username) && userPermissions[loggedUser.Username].Contains(category);
+        }
+
+        private bool UserCanAccessSubCategory(string category, string subCategory)
+        {
+            return userPermissions.ContainsKey(loggedUser.Username) && userPermissions[loggedUser.Username].Contains(subCategory);
+        }
+
+        private void BtnSettings_Click(object sender, EventArgs e)
+        {
+            SettingsForm settingsForm = new SettingsForm();
+            settingsForm.ShowDialog();
+            LogActivity("Abriu o painel de configurações.");
+        }
+
+        private void BtnLogout_Click(object sender, EventArgs e)
+        {
+            LogActivity("Logout");
+            UpdateUserLoginStatus(loggedUser.Username, false);
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UpdateUserLoginStatus(loggedUser.Username, false);
+            Application.Exit();
+        }
+
+        private void LogActivity(string activity)
+        {
+            string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {GetLocalIPAddress()} - {loggedUser.Username} - {activity}{Environment.NewLine}";
+            File.AppendAllText("activity_log.txt", logMessage);
+        }
+
+        private string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "Local IP Address Not Found!";
+        }
+
+        private void UpdateUserLoginStatus(string username, bool status)
+        {
+            var userLoginStatus = JsonConvert.DeserializeObject<Dictionary<string, bool>>(File.ReadAllText("userLoginStatus.json"));
+            userLoginStatus[username] = status;
+            File.WriteAllText("userLoginStatus.json", JsonConvert.SerializeObject(userLoginStatus, Formatting.Indented));
+        }
+
+        private void TextBoxSearch2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBoxSubCategory2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
